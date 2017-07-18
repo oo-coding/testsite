2103c6dfbfcb18c8dad2e3e4099d1769a4fd655c
Sh <shardest9999@yahoo.com>
2017-07-18 19:58:06 -0400

    Added models

--- app/models/Supdate.php
+++ app/models/Supdate.php
@@ -4,9 +4,7 @@ namespace PMAN\models;
 use Illuminate\Database\Eloquent\Model as Eloquent;
 
 //Eloquent expects table to be plural in lowercase, and class to be singular in Titlecase
-class Tmp extends Eloquent
+class Supdate extends Eloquent
 {
 
-    //Eloquent expects created_at, updated_at fields
-    public $timestamps = false;
 }
